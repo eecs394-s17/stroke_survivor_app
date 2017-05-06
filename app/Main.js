c9cae28497388374830eb57658ce4c67b8e0b0d9
sgp715 <7sebastianperez@gmail.com>
2017-05-06 12:30:58 -0500

    triying roll and pitch

--- app/Main.js
+++ app/Main.js
@@ -9,6 +9,8 @@ import {
   View
 } from 'react-native';
 
+
+
 export default class App extends Component {
 
   constructor(props) {
@@ -19,21 +21,59 @@ export default class App extends Component {
     // });
   }
 
+  calculate_rp(x, y, z) {
+
+
+
+    var rp = {
+      pitch: Math.atan2(-1 * x, Math.sqrt(Math.pow(y, 2) + Math.pow(z, 2))) * 57.3,
+      roll: Math.atan2(y, z) * 57.3,
+    }
+
+    return rp
+
+  }
+
   render() {
+
+    // int x, y, z;                        //three axis acceleration data
+    // double roll = 0.00, pitch = 0.00;        //Roll & Pitch are the angles which rotate by the axis X and y
+    //
+    // void RP_calculate(){
+    //   double x_Buff = float(x);
+    //   double y_Buff = float(y);
+    //   double z_Buff = float(z);
+    //   roll = atan2(y_Buff , z_Buff) * 57.3;
+      // pitch = atan2((- x_Buff) , sqrt(y_Buff * y_Buff + z_Buff * z_Buff)) * 57.3;
+    // }
 
     const accelerationObservable = new Accelerometer({
        updateInterval: 100,
     });
 
+
+    const gyroscopeObservable = new Gyroscope({
+      updateInterval: 1000,
+    })
+
+
+    // .map(({ x, y, z }) => x + y + z )
+    // .filter(speed => speed > 5)
+
     console.log('Initialized accelerometer!');
     accelerationObservable
-      .map(({ x, y, z }) => x )
-      .filter(speed => speed > 5)
-      .subscribe(speed => console.log(`You moved your phone with ${speed}`));
+      .subscribe(axis => console.log("axis: ", this.calculate_rp(axis.x, axis.y, axis.z)));
 
     setTimeout(() => {
       accelerationObservable.stop();
     }, 100000);
+
+    // gyroscopeObservable
+    //   .subscribe(gyro => console.log("gyro: ", gyro));
+    //
+    // setTimeout(() => {
+    //   gyroscopeObservable.stop();
+    // }, 100000);
 
     return (
       <Text>
@@ -45,7 +85,7 @@ export default class App extends Component {
       //     </Stage>
       //   </Loop>
       );
-      
+
   }
 
 }
