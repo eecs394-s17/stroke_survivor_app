c3fbffb0d403f70611d0d22faac4ab141e41e8ae
Robert Jin <dajunjin2016@u.northwestern.edu>
2017-05-07 13:52:57 -0500

    moving ball w/ collision

--- app/src/index.js
+++ app/src/index.js
@@ -12,19 +12,40 @@ import Matter from 'matter-js';
 export default class Game extends Component {
 
   handleUpdate = () => {
+
+
+
+
     this.setState({
       ballPosition: this.body.body.position,
       ballAngle: this.body.body.angle,
     });
-    Matter.Body.setVelocity(this.body.body, {x:1, y:0});
-    Matter.Body.setAngularVelocity(this.body.body, 0.1);
+
+    Matter.Body.applyForce(this.body.body, {x:this.body.body.position.x,  y:this.body.body.position.y}, {x:this.state.direction*0.0075, y:0});
+    Matter.Body.setAngularVelocity(this.body.body, this.state.direction*0.1);
+
+  }
+
+  handleCollision = (events) => {
+
+    // console.log(events.pairs);
+    for (var i = 0; i < events.pairs.length; i++){
+      console.log(events.pairs[i].id);
+      if (events.pairs[i].id === "1_5" || events.pairs[i].id === "1_4")
+      { 
+      // 1_5 and 1_4 refer to pairs of bodies with ids 1,5 and 1,4, respectively. The ball has an id of 1, the right wall has an id of 5, the left wall has an id of 4
+        this.setState({direction: -1*this.state.direction});
+        console.log("changing direction");
+      }
+
+    }
 
   }
 
   physicsInit = (engine) => {
 
     const dimensions = Dimensions.get('window');
-
+    console.log(dimensions);
 
     const ground = Matter.Bodies.rectangle(
       dimensions.width / 2, dimensions.height + 5,
@@ -58,12 +79,14 @@ export default class Game extends Component {
       },
     );
 
+
     Matter.World.add(engine.world, [ground, leftWall, rightWall, ceiling]);
+
+
   }
 
   constructor(props) {
     super(props);
-
 
     this.state = {
       gravity: 1,
@@ -72,54 +95,52 @@ export default class Game extends Component {
         y: 0,
       },
       ballAngle: 0,
+      direction: 1,
     };
   }
 
   componentWillMount() {
-    this._panResponder = PanResponder.create({
-      onStartShouldSetPanResponder: (evt, gestureState) => true,
-      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
-      onMoveShouldSetPanResponder: (evt, gestureState) => true,
-      onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
-      onPanResponderGrant: (evt, gestureState) => {
-        this.setState({
-          gravity: 0,
-        });
 
-        Matter.Body.setAngularVelocity(this.body.body, 0);
-        Matter.Body.setVelocity(this.body.body, {x: 0, y: 0});
+    // this._panResponder = PanResponder.create({
+    //   onStartShouldSetPanResponder: (evt, gestureState) => true,
+    //   onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+    //   onMoveShouldSetPanResponder: (evt, gestureState) => true,
+    //   onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+    //   onPanResponderGrant: (evt, gestureState) => {
+    //     this.setState({
+    //       gravity: 0,
+    //     });
 
-        this.startPosition = {
-          x: this.body.body.position.x,
-          y: this.body.body.position.y,
-        }
-      },
-      onPanResponderMove: (evt, gestureState) => {
-        Matter.Body.setPosition(this.body.body, {
-          x: this.startPosition.x + gestureState.dx,
-          y: this.startPosition.y + gestureState.dy,
-        });
-      },
-      onPanResponderRelease: (evt, gestureState) => {
-        this.setState({
-          gravity: 1,
-        });
+    //     Matter.Body.setAngularVelocity(this.body.body, 0);
+    //     Matter.Body.setVelocity(this.body.body, {x: 0, y: 0});
 
-        Matter.Events.on(engine, 'collisionActive', function(){
-          console.log('testing');
-        });
+    //     this.startPosition = {
+    //       x: this.body.body.position.x,
+    //       y: this.body.body.position.y,
+    //     }
+    //   },
+    //   onPanResponderMove: (evt, gestureState) => {
+    //     Matter.Body.setPosition(this.body.body, {
+    //       x: this.startPosition.x + gestureState.dx,
+    //       y: this.startPosition.y + gestureState.dy,
+    //     });
+    //   },
+    //   onPanResponderRelease: (evt, gestureState) => {
+    //     this.setState({
+    //       gravity: 1,
+    //     });
 
-        console.log(engine);
 
-        Matter.Body.applyForce(this.body.body, {
-          x: this.body.body.position.x,
-          y: this.body.body.position.y,
-        }, {
-          x: gestureState.vx,
-          y: gestureState.vy,
-        });
-      },
-    });
+
+    //     // Matter.Body.applyForce(this.body.body, {
+    //     //   x: this.body.body.position.x,
+    //     //   y: this.body.body.position.y,
+    //     // }, {
+    //     //   x: gestureState.vx,
+    //     //   y: gestureState.vy,
+    //     // });
+    //   },
+    // });
   }
 
   getBallStyles() {
@@ -147,25 +168,27 @@ export default class Game extends Component {
           <World
             onInit={this.physicsInit}
             onUpdate={this.handleUpdate}
+            onCollision={this.handleCollision}
             gravity={{ x: 0, y: this.state.gravity, scale: 0.001 }}
           >
             <Body
               shape="circle"
               args={[0, dimensions.height - 75, 75]}
               density={0.003}
-              friction={1}
+              friction={0}
               frictionStatic={0}
-              restitution={0.5}
+              restitution={0}
               ref={(b) => { this.body = b; }}
             >
               <View
-                style={this.getBallStyles()} {...this._panResponder.panHandlers}
+                style={this.getBallStyles()} //{...this._panResponder.panHandlers}
               >
                 <Image
                   source={require('./assets/basketball.png')}
                   height={75}
                   width = {75}
                 />
+
               </View>
             </Body>
           </World>
