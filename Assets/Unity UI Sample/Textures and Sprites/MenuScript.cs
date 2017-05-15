0e62198dcc26cf4ca2f1da62f35e542dd9deb4de
rawitscher <lindsayrawitscher2017@u.northwestern.edu>
2017-05-15 17:19:25 -0500

    navigation fully functional - no UI

---
+++ Assets/Unity UI Sample/Textures and Sprites/MenuScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+	// Load an assetbundle which contains scenes.
+	// When the user clicks a button the first scene in the assetbundle is
+	// loaded and replaces the current scene.
+
+
+		private AssetBundle myLoadedAssetBundle;
+		private string[] scenePaths;
+
+		// Use this for initialization
+		void Start()
+		{
+			myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/AssetBundles/scenes");
+			scenePaths = myLoadedAssetBundle.GetAllScenePaths();
+		}
+
+		void OnGUI()
+		{
+			if (GUI.Button(new Rect(10, 10, 100, 30), "Change scene"))
+			{
+				Debug.Log("scene2 loading: " + scenePaths[0]);
+				SceneManager.LoadScene(scenePaths[0], LoadSceneMode.Single);
+			}
+
+	}
+}
+
+
